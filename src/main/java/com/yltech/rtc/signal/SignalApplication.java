f714e1eed53cc5db9c40177ef6906fb3e8e94148
wanghuan578 <57810140@qq.com>
2020-05-15 16:14:59 +0800

    1

--- src/main/java/com/yltech/rtc/signal/SignalApplication.java
+++ src/main/java/com/yltech/rtc/signal/SignalApplication.java
@@ -51,7 +51,7 @@ public class SignalApplication {
         config.setKeyStore(key);
         config.setKeyStorePassword("CFj17tUl");
 
-        config.setHostname("signal.keepfun.cn");
+        config.setHostname("localhost");
         config.setPort(443);
         SocketIOServer server = new SocketIOServer(config);
         server.addConnectListener(new ConnectListener() {
